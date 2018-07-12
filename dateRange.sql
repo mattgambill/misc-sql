d87be8a4b6f2e800a4b6db17d98f75574bb0dfc8
mattgambill <gambill.46@osu.edu>
2018-07-12 14:44:07 -0500

    Create dateRange.sql

---
+++ dateRange.sql
@@ -0,0 +1,20 @@
+/*
+Snippet to Select a date Range For Alarms
+By Matt Gambill, 7/12/2018
+Synopsis:
+Snippet Takes the record in a table with the latest date,
+then filters the table for a date range between the latest date,
+and a specified interval (3 days ago).
+
+
+
+*/
+/**** Snippet to Select a date Range ****/
+
+  DECLARE @to_date datetime = (SELECT max(EventStamp) from [WWALMDB].[dbo].[v_AlarmEventHistory]);
+  DECLARE @interval_length int  = -3; /* Must Be a negative integer */
+  DECLARE @from_date datetime = DATEADD(day, @interval_length, @to_date);
+  
+  SELECT [EventStamp],[TagName],[Description] 
+  FROM [WWALMDB].[dbo].[v_AlarmEventHistory]
+  WHERE [EventStamp] <= @to_date AND [EventStamp] >= @from_date;
